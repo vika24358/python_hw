fefb8a30438668719c63561ca071d4712e380268
vikusik <me@example.com>
2024-06-09 14:40:57 +0300

    branching start + distance app development

---
+++ branching.py
@@ -0,0 +1,22 @@
+# True
+real_true = True
+
+if real_true:
+    print('true')
+
+# compare
+compare_result1 = 1 == 2  # False
+false = False
+
+if compare_result1:
+    print(444444)
+    print('goodbye')
+
+if not compare_result1:
+    print(44949949494)
+    print('hello')
+
+compare_two_false = compare_result1 == false
+print(compare_two_false)
+
+correct_false_comparison = false is compare_result1
