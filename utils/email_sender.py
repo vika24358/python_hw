efee2529059754b811685143d128f1cab0fd8554
vikusik <me@example.com>
2024-07-20 17:35:16 +0300

    error

--- utils/email_sender.py
+++ utils/email_sender.py
@@ -2,20 +2,22 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import smtplib
 
+import jinja2
+
 import config
 
 
 def send_email(
-    *,
-    recipients: list[str],
-    mail_body: str,
-    mail_subject: str,
-    attachment: str = None,
+        *,
+        recipients: list[str],
+        mail_body: str,
+        mail_subject: str,
+        attachment: str = None,
 ):
     TOKEN = config.TOKEN_UKR_NET
     USER = config.MAIL_USER
     SMTP_SERVER = config.SMTP_SERVER
-
+    print()
     msg = MIMEMultipart('alternative')
     msg['Subject'] = mail_subject
     msg['From'] = f'<Email was sent from {USER}>'
@@ -29,5 +31,14 @@ def send_email(
 
     mail = smtplib.SMTP_SSL(SMTP_SERVER)
     mail.login(USER, TOKEN)
-    mail.sendmail(USER, recipients, msg.as_string())
+    mail.sendmail(from_addr=USER, to_addrs=recipients, msg=msg.as_string())
     mail.quit()
+
+
+def create_welcome_letter(params: dict) -> str:
+    template_loader = jinja2.FileSystemLoader(searchpath='./')
+    template_env = jinja2.Environment(loader=template_loader)
+    template_file = 'templates/letter_template.html'
+    template = template_env.get_template(template_file)
+    output = template.render(params)
+    return output
